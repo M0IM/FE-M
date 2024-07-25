f5b0fdb122f4245b3b5a0d42404313956e9c59d3
김용민 <dydals3440@gmail.com>
2024-07-25 15:26:26 +0900

    feat: Tailwind css Setting, Font / Color Setting

--- src/screens/FeedTabScreens/FeedHomeScreen.tsx
+++ src/screens/FeedTabScreens/FeedHomeScreen.tsx
@@ -5,9 +5,10 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 export default function FeedHomeScreen() {
   return (
     <View>
-      <Text>FEED_HOME 화면입니다.</Text>
+      <Text className="footnote">FEED_HOME 화면입니다.</Text>
       <CatSvg width={200} height={200} fill="#fff" />
-      <Text>HI</Text>
+      <Text className="font-bold">HI</Text>
+      <Text className={'index'}></Text>
       <Icon name="delete" size={32} color="red" />
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const nativewind = require('nativewind/tailwind/native');
 
 module.exports = {
+  darkMode: ['class'],
   content: [
     './App.{js,jsx,ts,tsx}',
     './src/screens/**/*.{ts,tsx}',
@@ -9,6 +10,40 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      gray: {
+        50: '#FCFCFC',
+        100: '#F8F8F9',
+        200: '#E9ECEF',
+        300: '#C9CCD1',
+        400: '#9EA4AA',
+        500: '#72787F',
+        600: '#454C53',
+      },
+      dark: {
+        800: '#26282B',
+        900: '#1D2002',
+      },
+      green: {
+        50: '#FBFFF5',
+        100: '#F2FFE0',
+        200: '#E3FCBF',
+        300: '#CBFAC4',
+        400: '#B8F1B0',
+        500: '#84DE77',
+        600: '#14C38E',
+      },
+      white: '#FFFFFF',
+      black: '#000000',
+      error: '#ff3E3E',
+      warning: '#FFB800',
+      main: '#00F0A1',
+    },
+    fontFamily: {
+      bold: ['Pretendard-Bold'],
+      light: ['Pretendard-Light'],
+      medium: ['Pretendard-Medium'],
+    },
   },
   plugins: [nativewind()],
 };

bc313388f62cdaca2a7c9c5e9377b68b1f6b5c78
김용민 <dydals3440@gmail.com>
2024-07-25 14:14:24 +0900

    feat: eslint 세팅

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/ban-ts-comment": 'off',
+      "no-var": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "no-undef": "off",
+      '@typescript-eslint/ban-types': 'off',
+      'react/no-unstable-nested-components': 'off',
+      'react-native/no-inline-styles': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    }
+  }
+];

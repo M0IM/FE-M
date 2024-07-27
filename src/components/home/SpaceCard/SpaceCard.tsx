17896769306fcd0aef1a51f9ee4f93e5d9fb8f3b
daindaind <cdin23@naver.com>
2024-07-27 13:01:51 +0900

    MM-37 Feat: RecommendBar Component 구현, SpaceCard Style 수정

--- src/components/home/SpaceCard/SpaceCard.tsx
+++ src/components/home/SpaceCard/SpaceCard.tsx
@@ -16,11 +16,11 @@ const SpaceCard = ({
     <TouchableOpacity
       {...props}
       activeOpacity={0.8}
-      className='flex flex-col items-center gap-3 w-[160]'
+      className='flex flex-col items-center gap-3 w-[140]'
     >
-      {uri ? <Image source={{uri: uri}} width={140} height={120} className='rounded-[30px]' /> :
+      {uri ? <Image source={{uri: uri}} width={120} height={100} className='rounded-[30px]' /> :
         (
-          <View className='flex flex-col justify-center items-center w-[140] h-[120] rounded-[20px] bg-gray-100'>
+          <View className='flex flex-col justify-center items-center w-[120] h-[100] rounded-[30px] bg-gray-100'>
             <Ionicons name='home' color='#E9ECEF' size={50} />
           </View>
         )

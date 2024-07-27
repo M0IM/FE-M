a9b608f5623f606f34219ea3861a1a31e0b3e5de
김용민 <dydals3440@gmail.com>
2024-07-27 18:25:03 +0900

    feat: InputField 컴포넌트 생성

---
+++ src/components/@common/InputField/InputField.tsx
@@ -0,0 +1,11 @@
+import {ForwardedRef, ReactNode, forwardRef, useRef} from 'react';
+import {
+  Dimensions,
+  Pressable,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from 'react-native';
+
+export const InputField = ({}) => {};

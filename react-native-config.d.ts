5bf5dbcee9f0f3bb1328b155517188f070180ed2
김용민 <dydals3440@gmail.com>
2024-07-27 16:24:10 +0900

    [MM-36] Social Login Apple 구현 (#9)
    * fix: iOS/AOS 카카오 로그인 에러 해결
    
    * feat: Apple Login 구현 및 Social Image 파일 변경

--- react-native-config.d.ts
+++ react-native-config.d.ts
@@ -1,6 +1,7 @@
 declare module 'react-native-config' {
   export interface NativeConfig {
     KAKAO_NATIVE_APP_KEY?: string;
+    KAKAO_WITH_NATIVE_APP_KEY?: string;
     KAKAO_REST_API_KEY?: string;
     KAKAO_JS_KEY?: string;
     KAKAO_ADMIN_KEY?: string;

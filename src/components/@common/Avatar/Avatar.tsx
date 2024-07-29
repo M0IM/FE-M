ebda6c97a02f8e8b802990d3400e42b4dbec317e
daindaind <cdin23@naver.com>
2024-07-29 22:17:16 +0900

    MM-45 Feat: Avatar 컴포넌트 구현

---
+++ src/components/@common/Avatar/Avatar.tsx
@@ -0,0 +1,67 @@
+import { Image, ImageProps } from 'react-native';
+import React from 'react';
+import { cva } from 'class-variance-authority';
+import { cn } from 'utils';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+enum SIZE {
+    XS = 25,
+    SM = 40,
+    MD = 60,
+    LG = 100
+}
+
+interface AvatarProps extends ImageProps {
+    uri?: string;
+    size?: 'XS' | 'SM' | 'MD' | 'LG',
+    iconColor?: string; 
+}
+
+const Avatar = ({
+  uri,
+  size = 'SM',
+  iconColor = '#E9ECEF',
+  className,
+  ...props
+}: AvatarProps) => {
+  const iconSizes = {
+    XS: SIZE.XS,
+    SM: SIZE.SM,
+    MD: SIZE.MD,
+    LG: SIZE.LG,
+  };
+    
+  const userIconSize = iconSizes[size] || iconSizes.SM;
+
+  return (
+    <>
+      {uri ? 
+        <Image
+          source={{uri: uri}}
+          className={cn(avatarVariants({size}), className)}
+          {...props}
+        /> : (
+          <Ionicon name="person-circle" size={userIconSize} color={iconColor} />
+        )}
+    </>
+  );
+};
+
+const avatarVariants = cva(
+  'flex flex-col items-center justify-center bg-gray-100 rounded-full',
+  {
+    variants: {
+      size: {
+        XS: `w-[${SIZE.XS}] h-[${SIZE.XS}]`,
+        SM: `w-[${SIZE.SM}] h-[${SIZE.SM}]`,
+        MD: `w-[${SIZE.MD}] h-[${SIZE.MD}]`,
+        LG: `w-[${SIZE.LG}] h-[${SIZE.LG}]`,
+      },
+      defaultVariants: {
+        size: 'SM',
+      }
+    }
+  }
+);
+
+export default Avatar;

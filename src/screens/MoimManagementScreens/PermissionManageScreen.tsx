e70e13057b0078f3cf2291e5de746ccf0401d9e7
daindaind <cdin23@naver.com>
2024-08-16 23:18:12 +0900

    Feat: 모임 가입 관리 UI

--- src/screens/MoimManagementScreens/PermissionManageScreen.tsx
+++ src/screens/MoimManagementScreens/PermissionManageScreen.tsx
@@ -11,18 +11,21 @@ const testUserData = [
   {
     id: 1,
     username: '차다인',
+    profileImg: '',
     role: '모임장',
     isPermissioned: true,
   },
   {
     id: 2,
     username: '차다인',
+    profileImg: '',
     role: '관리자',
     isPermissioned: true,
   },
   {
     id: 3,
     username: '차다인',
+    profileImg: '',
     role: '부원',
     isPermissioned: false,
   }
@@ -49,11 +52,11 @@ const PermissionManageScreen = ({route}: PermissionManageScreenProps) => {
 
       {testUserData.map((item) => (
         <View key={item.id} className='flex flex-row items-center'>
-          <Avatar />
+          <Avatar uri={item.profileImg} />
           <Typography fontWeight='MEDIUM' className='text-dark-800 text-sm ml-3 mr-3'>{item.username}</Typography>
           {item.role !== '모임장' && <Label label={item.role} color="dark" />}
           <TouchableOpacity className='p-2 rounded-xl bg-gray-200 ml-auto'>
-            <Typography fontWeight='MEDIUM' className='text-gray-500 text-xs'>{item.isPermissioned ? '권한 취소' : '권한 부여'}</Typography>
+            <Typography fontWeight='MEDIUM' className='text-gray-600 text-xs'>{item.isPermissioned ? '권한 취소' : '권한 부여'}</Typography>
           </TouchableOpacity>
         </View>
       ))}

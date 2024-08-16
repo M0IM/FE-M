76ff7429e5390924419da6b6868f9d801d6a4172
daindaind <cdin23@naver.com>
2024-08-17 01:01:22 +0900

    Feat: 모임 수정 드롭다운 UI 구현

--- src/components/screens/MoimCreateScreen/CategoryDropdown.tsx
+++ src/components/screens/MoimCreateScreen/CategoryDropdown.tsx
@@ -1,19 +1,71 @@
-import { Typography } from 'components/@common/Typography/Typography';
-import { TouchableOpacity, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Animated, Easing, ScrollView, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Typography } from 'components/@common/Typography/Typography';
 
-const CategoryDropdown = () => {
- 
-    // 추후에 바텀모달로 변경
+interface CategoryDropdownProps extends TouchableOpacityProps {
+    isPressed: boolean;
+    menuList: string[];
+    handleSelect: Function;
+    selectedMenu: string;
+    placeholder?: string;
+}
+
+const CategoryDropdown = ({
+    isPressed,
+    menuList,
+    handleSelect,
+    selectedMenu,
+    placeholder,
+    ...props
+}: CategoryDropdownProps) => {
+    const dropdownHeight = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        Animated.timing(dropdownHeight, {
+            toValue: isPressed ? 300 : 0,
+            duration: 300,
+            easing: Easing.ease,
+            useNativeDriver: false,
+        }).start();
+    }, [isPressed]);
+
+    const padding = dropdownHeight.interpolate({
+        inputRange: [0, 400],
+        outputRange: [0, 12],
+    });
+    
     return (
         <View className='flex flex-col'>
             <Typography fontWeight={'MEDIUM'} className='text-sm text-gray-500 mb-2'>
                 카테고리
             </Typography>
-            <TouchableOpacity activeOpacity={0.8} className='flex flex-row border-0.5 border-gray-100 rounded-xl bg-gray-100 p-4'>
-                <Typography fontWeight='MEDIUM' className='text-gray-500'>카테고리</Typography>
+            <TouchableOpacity
+                {...props}
+                activeOpacity={0.8} 
+                className='flex flex-row border-0.5 border-gray-100 rounded-xl bg-gray-100 p-4 mb-4'
+            >
+                <Typography 
+                    fontWeight='MEDIUM' 
+                    className={selectedMenu ? 'text-dark-800' : 'text-gray-500'}
+                >
+                    {selectedMenu ? selectedMenu : placeholder }
+                </Typography>
                 <Ionicons name="chevron-down" style={{marginLeft: 'auto'}} size={15} color={'#535353'} />
-            </TouchableOpacity> 
+            </TouchableOpacity>
+
+            <Animated.View 
+                style={{ height: dropdownHeight, paddingHorizontal: 12, paddingVertical: padding }}
+                className='flex flex-col bg-gray-100 rounded-xl pt-0 gap-y-2'
+            >
+                <ScrollView nestedScrollEnabled>
+                    {menuList.map((item, index) => (
+                        <TouchableOpacity key={index} onPress={() => handleSelect(item)} className='p-2'>
+                            <Typography fontWeight='MEDIUM' className='text-dark-800 text-sm'>{item}</Typography>
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
+            </Animated.View>
         </View>
     );
 };
--- src/screens/MoimManagementScreens/MoimInfoEditScreen.tsx
+++ src/screens/MoimManagementScreens/MoimInfoEditScreen.tsx
@@ -1,18 +1,138 @@
+import { View, TouchableOpacity, Image, Platform } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { CustomButton } from 'components/@common/CustomButton/CustomButton';
+import { ImageInput } from 'components/@common/ImageInput/ImageInput';
+import { InputField } from 'components/@common/InputField/InputField';
+import { Typography } from 'components/@common/Typography/Typography';
+import { ScreenContainer } from 'components/ScreenContainer';
+import CategoryDropdown from 'components/screens/MoimCreateScreen/CategoryDropdown';
+import MoimTagContainer from 'components/screens/MoimCreateScreen/MoimTagContainer';
+import useImagePicker from 'hooks/useImagePicker';
+import usePermission from 'hooks/usePermission';
+import useTags from 'hooks/useTags';
 import { MoimManagementRouteProp } from 'navigators/types';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { CATEGORY_MENU_LIST } from 'constants/screens/MoimSearchScreen/CategoryList';
 
 interface MoimInfoEditScreenProps {
   route: MoimManagementRouteProp;
 }
 
 const MoimInfoEditScreen = ({route}: MoimInfoEditScreenProps) => {
+  const {tags, addTagField, handleTagChange, removeTagField} = useTags();
+  const platform = Platform.OS;
   const moimdId = route.params.id;
+  const [isPressed, setIsPressed] = useState(false);
+  const [category, setCategory] = useState('');
   console.log('moimId: ', moimdId);
 
+  const handleSelectedCategory = (selected: any) => {
+    setCategory(selected);
+  };
+
+  const handleCategory = () => {
+    setIsPressed(prev => !prev);
+  };
+
+  const imagePicker = useImagePicker({
+    initialImages: [],
+  });
+  usePermission('PHOTO');
+
+  const hahndleOnSubmit = () => {
+    console.log(category);
+  };
+
   return (
-    <View>
-      <Text>MoimInfoEditScreen</Text>
-    </View>
+    <ScreenContainer
+      fixedBottomComponent={
+        <CustomButton
+          label="모임 정보 수정"
+          textStyle="text-white font-bold text-base"
+          onPress={hahndleOnSubmit}
+        />
+      }>
+      <View className="flex flex-col">
+        <Typography
+          fontWeight={'MEDIUM'}
+          className="text-sm text-gray-500 mb-2 mt-4">
+          모임 이름
+        </Typography>
+        <InputField touched placeholder="모임 이름 입력" />
+      </View>
+      <View className="flex flex-col">
+        <Typography
+          fontWeight={'MEDIUM'}
+          className="text-sm text-gray-500 mb-2">
+          활동 지역
+        </Typography>
+        <View className="flex flex-row w-full items-center justify-around gap-x-2">
+          <View className="flex-1">
+            <InputField touched placeholder="활동 지역 찾기" />
+          </View>
+          <TouchableOpacity activeOpacity={0.8}>
+            <Ionicons name="search" size={30} color={'#C9CCD1'} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* 카테고리 드롭다운 */}
+      <CategoryDropdown 
+        onPress={handleCategory} 
+        isPressed={isPressed} 
+        menuList={CATEGORY_MENU_LIST}
+        handleSelect={handleSelectedCategory}
+        selectedMenu={category}
+        placeholder='카테고리'
+      />
+
+      {/* 카메라 연결 */}
+      <View className="flex flex-col">
+        <Typography
+          fontWeight={'MEDIUM'}
+          className="text-sm text-gray-500 mb-2">
+          대표 이미지
+        </Typography>
+        <ImageInput onChange={imagePicker.handleChange} />
+        {/* TODO: PreviewImage Component 분리 (실제 DB 연결이후) */}
+        <View>
+          {imagePicker.imageUris.map(({uri}, index) => {
+            return (
+              <Image
+                key={index}
+                source={{
+                  uri: `http:s3address/${uri}`,
+                }}
+              />
+            );
+          })}
+        </View>
+      </View>
+
+      <View className="flex flex-col">
+        <Typography
+          fontWeight={'MEDIUM'}
+          className="text-sm text-gray-500 mb-2">
+          모임 소개
+        </Typography>
+        <View className="max-h-[800px]">
+          <InputField touched placeholder="모임 소개 입력" multiline />
+        </View>
+      </View>
+
+      {/* 태그 컴포넌트 */}
+      <MoimTagContainer
+        tags={tags}
+        addTagField={addTagField}
+        removeTagField={removeTagField}
+        handleTagChange={handleTagChange}
+      />
+
+      {/* 모임 소개 영상 게시 */}
+      {/* <MoimIntroVideo /> */}
+
+      <View className={platform === 'android' ? 'mt-16' : 'mt-6'} />
+    </ScreenContainer>
   );
 };
 

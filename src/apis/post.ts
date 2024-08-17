65fa1bdc32f6251bafe2867f8d336e22fc8e1bbf
daindaind <cdin23@naver.com>
2024-08-18 05:51:53 +0900

    Feat: 모임 게시글 관련 api 작성

---
+++ src/apis/post.ts
@@ -0,0 +1,119 @@
+import { 
+    TMoimPostCommentParams,
+    TMoimPostCommentsParams,
+    TMoimPostDetailParams,
+    TMoimPostListParams,
+    TMoimPostParams,
+    TMoimPostRecommentParams,
+    TPostCommentListDto,
+    TPostDetailDto,
+    TPostDto,
+    TPostListDto
+} from "types/dtos/post";
+import axiosInstance from "./axiosInstance";
+
+const writeMoimPost = async ({
+    moimId,
+    title,
+    content,
+    imageKeyNames,
+    postType
+}: TMoimPostParams): Promise<TPostDto> => {
+    const { data } = await axiosInstance.post('/api/v1/moims/posts', {
+        moimId,
+        title,
+        content,
+        imageKeyNames,
+        postType
+    });
+    console.log(data);
+    return data;
+};
+
+const likeMoimPost = async ({postId}: {postId: number}) => {
+    const { data } = await axiosInstance.post('/api/v1/moims/posts/like', {
+        postId
+    });
+    console.log(data);
+    return data;
+};
+
+const writeMoimPostComment = async ({
+    moimId,
+    postId,
+    content
+}: TMoimPostCommentParams): Promise<TPostDto> => {
+    const { data } = await axiosInstance.post('/api/v1/moims/posts/comments', {
+        moimId,
+        postId,
+        content
+    });
+    console.log(data);
+    return data;
+};
+
+const writeMoimPostRecomment = async ({
+    moimId,
+    commentId,
+    postId,
+    content
+}: TMoimPostRecommentParams): Promise<TPostDto> => {
+    const { data } = await axiosInstance.post('/api/v1/moims/posts/comments/comments', {
+        moimId,
+        commentId,
+        postId,
+        content
+    });
+    console.log(data);
+    return data;
+};
+
+const likeMoimPostComment = async ({commentId}: {commentId: number}) => {
+    const { data } = await axiosInstance.post('/api/v1/moims/comments/Like', {
+        commentId
+    });
+    console.log(data);
+    return data;
+};
+
+const getMoimPostList = async ({
+    moimId,
+    postType,
+    cursor,
+    take
+}: TMoimPostListParams): Promise<TPostListDto> => {
+    const { data } = await axiosInstance.get(`/api/v1/moims/${moimId}/posts?postType=${postType}&cursor=${cursor}&take=${take}`);
+    console.log(data);
+    return data;
+};
+
+const getMoimPostDetail = async ({
+    moimId,
+    postId
+}: TMoimPostDetailParams): Promise<TPostDetailDto> => {
+    const { data } = await axiosInstance.get(`/api/v1/moims/${moimId}/posts/${postId}`);
+    console.log(data);
+    return data;
+};
+
+const getMoimPostComments = async ({
+    moimId,
+    postId,
+    cursor,
+    take
+}: TMoimPostCommentsParams): Promise<TPostCommentListDto> => {
+    const { data } = await axiosInstance.get(`/api/v1/moims/${moimId}/posts/${postId}/comments?cursor=${cursor}&take=${take}`);
+    console.log(data);
+    return data;
+};
+
+export {
+    writeMoimPost,
+    likeMoimPost,
+    writeMoimPostComment,
+    writeMoimPostRecomment,
+    likeMoimPostComment,
+    getMoimPostList,
+    getMoimPostDetail,
+    getMoimPostComments
+};
---
+++ src/types/dtos/post.ts
@@ -0,0 +1,126 @@
+enum POST_TYPE {
+    ANNOUNCEMENT = '공지사항',
+    REVIEW = '활동 후기',
+    WELCOME = '가입 인사',
+    FREE = '자유'
+}
+
+type TPostDto = {
+    moimPostId: number;
+    createAt: string;
+    updateAt: string;
+}
+
+type TMoimPreviewListDto = {
+    moimPostId: number;
+    title: string;
+    content: string;
+    profileImage: string;
+    writer: string;
+    commentCount: number;
+    likeCount: number;
+    postType: POST_TYPE;
+    createAt: string;
+}
+
+type TPostListDto = {
+    moimPreviewList: TMoimPreviewListDto[],
+    nextCursor: number;
+    hasNext: boolean;
+}
+
+type TPostDetailDto = {
+    moimPostId: number;
+    title: string;
+    content: string;
+    profileImage: string;
+    writer: string;
+    commentCount: number;
+    likeCount: number;
+    isLike: boolean;
+    imageKeyNames: string[];
+    updateAt: string;
+    createAt: string;
+}
+
+type TPostRecommentDto = {
+    commentId: number;
+    content: string;
+    likeCount: number;
+    profileImage: string;
+    writer: string;
+    isLike: boolean;
+    updateAt: string;
+    createAt: string; 
+}
+
+type TPostCommentDto = {
+    commentId: number;
+    content: string;
+    likeCount: number;
+    profileImage: string;
+    writer: string;
+    isLike: boolean;
+    updateAt: string;
+    createAt: string;
+    commentResponseDTOList: TPostRecommentDto[];
+}
+
+type TPostCommentListDto = {
+    moimPreviewList: TPostCommentDto[];
+    nextCursor: number;
+    hasNext: boolean;
+}
+
+type TMoimPostParams = {
+    moimId: number;
+    title: string;
+    content: string;
+    imageKeyNames: string[];
+    postType: POST_TYPE
+}
+
+type TMoimPostCommentParams = {
+    moimId: number;
+    postId: number;
+    content: string;
+}
+
+type TMoimPostRecommentParams = {
+    moimId: number;
+    commentId: number;
+    postId: number;
+    content: string;
+}
+
+type TMoimPostListParams = {
+    moimId: number;
+    postType: POST_TYPE;
+    cursor: number;
+    take: number;
+}
+
+type TMoimPostDetailParams = {
+    moimId: number;
+    postId: number;
+}
+
+type TMoimPostCommentsParams = {
+    moimId: number;
+    postId: number;
+    cursor: number;
+    take: number;
+}
+
+export type {
+    TMoimPostParams,
+    TMoimPostCommentParams,
+    TMoimPostRecommentParams,
+    TMoimPostListParams,
+    TMoimPostDetailParams,
+    TMoimPostCommentsParams,
+    TPostDto,
+    TPostListDto,
+    TPostDetailDto,
+    TPostCommentListDto
+};

00cceb2ea7cb233484ca7efc977e0405d638280d
김용민 <dydals3440@gmail.com>
2024-08-20 04:01:18 +0900

    feat: MoimPostWriteScreen refresh Logic 변경

--- src/screens/MoimBoardStackScreens/MoimBoardScreen.tsx
+++ src/screens/MoimBoardStackScreens/MoimBoardScreen.tsx
@@ -1,30 +1,28 @@
-import { useEffect, useState } from 'react';
-import { FlatList, Pressable, View } from 'react-native';
+import {useEffect, useState} from 'react';
+import {FlatList, Pressable, View} from 'react-native';
 import FloatingButton from 'components/@common/FloatingButton/FloatingButton';
-import { Typography } from 'components/@common/Typography/Typography';
+import {Typography} from 'components/@common/Typography/Typography';
 import BoardPostPreview from 'components/screens/MoimBoardStackScreens/BoardPostPreview';
-import { BOARD_TITLES } from 'constants/screens/MoimBoardStackScreens/PostList';
+import {BOARD_TITLES} from 'constants/screens/MoimBoardStackScreens/PostList';
 import usePost from 'hooks/queries/MoimBoard/usePost';
-import { MoimPostStackNavigationProp, MoimPostStackRouteProp } from 'navigators/types';
+import {
+  MoimPostStackNavigationProp,
+  MoimPostStackRouteProp,
+} from 'navigators/types';
 
-type BoardTitleType = typeof BOARD_TITLES[number]['key'];
+type BoardTitleType = (typeof BOARD_TITLES)[number]['key'];
 
 interface MoimBoardScreenProps {
   route: MoimPostStackRouteProp;
-  navigation: MoimPostStackNavigationProp
+  navigation: MoimPostStackNavigationProp;
 }
 
 const MoimBoardScreen = ({route, navigation}: MoimBoardScreenProps) => {
   const [isSelected, setIsSelected] = useState<BoardTitleType>('ALL');
   const [isRefreshing, setIsRefreshing] = useState(false);
-  const { useGetInfiniteMoimPostList } = usePost();
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    refetch
-  } = useGetInfiniteMoimPostList(route?.params?.id as number, isSelected);
+  const {useGetInfiniteMoimPostList} = usePost();
+  const {data, fetchNextPage, hasNextPage, isFetchingNextPage, refetch} =
+    useGetInfiniteMoimPostList(route?.params?.id as number, isSelected);
   console.log('board route', route);
 
   const handleEndReached = () => {
@@ -43,47 +41,49 @@ const MoimBoardScreen = ({route, navigation}: MoimBoardScreenProps) => {
     setIsSelected(selectMenu);
   };
 
-  useEffect(() => {
-    refetch();
-  }, [isSelected]);
-
   return (
     <>
-      <View className='flex flex-row justify-around border-b-[1px] border-gray-200 px-3 mb-3'>
-        {BOARD_TITLES.map(({ key, label }) => (
+      <View className="flex flex-row justify-around border-b-[1px] border-gray-200 px-3 mb-3">
+        {BOARD_TITLES.map(({key, label}) => (
           <Pressable
             key={key}
             onPress={() => handleSelect(key)}
-            className={`${isSelected === key ? 'border-b-2 border-dark-800' : ''}`}
-          >
-            <Typography fontWeight='BOLD' className={`text-gray-200 text-base p-2 ${isSelected === key ? 'text-dark-800' : ''}`}>
+            className={`${isSelected === key ? 'border-b-2 border-dark-800' : ''}`}>
+            <Typography
+              fontWeight="BOLD"
+              className={`text-gray-200 text-base p-2 ${isSelected === key ? 'text-dark-800' : ''}`}>
               {label}
             </Typography>
           </Pressable>
         ))}
       </View>
-      <FlatList 
-        data={data?.pages.flat()}
+      <FlatList
+        data={data?.pages.flatMap(data => data.moimPreviewList)}
         renderItem={({item}) => (
-          <FlatList 
-            data={item.moimPreviewList}
-            renderItem={({item}) => (
-              <BoardPostPreview moimId={route?.params.id} postPreview={item} navigation={navigation} />
-            )}
-            ItemSeparatorComponent={() => <View className='h-3' />}
-            keyExtractor={item => String(item?.moimPostId)}
-            numColumns={1}
-            onEndReached={handleEndReached}
-            onEndReachedThreshold={0.5}
-            refreshing={isRefreshing}
-            onRefresh={handleRefresh}
+          <BoardPostPreview
+            moimId={route?.params.id}
+            postPreview={item}
+            navigation={navigation}
           />
         )}
+        ItemSeparatorComponent={() => <View className="h-3" />}
+        keyExtractor={item => String(item?.moimPostId)}
+        numColumns={1}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.5}
+        refreshing={isRefreshing}
+        onRefresh={handleRefresh}
         contentContainerStyle={{
-          paddingHorizontal: 15,
+          paddingHorizontal: 20,
+          gap: 10,
         }}
       />
-      <FloatingButton type='add' onPress={() => navigation.navigate('MOIM_POST_WRITE', { id: route.params.id })} />
+      <FloatingButton
+        type="add"
+        onPress={() =>
+          navigation.navigate('MOIM_POST_WRITE', {id: route.params.id})
+        }
+      />
     </>
   );
 };
--- src/screens/MoimBoardStackScreens/MoimPostWriteScreen.tsx
+++ src/screens/MoimBoardStackScreens/MoimPostWriteScreen.tsx
@@ -1,25 +1,36 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import Toast from 'react-native-toast-message';
-import { FlatList, Image, Pressable, TextInput, TouchableOpacity, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { InputField } from 'components/@common/InputField/InputField';
-import { Typography } from 'components/@common/Typography/Typography';
-import { ScreenContainer } from 'components/ScreenContainer';
-import { CustomButton } from 'components/@common/CustomButton/CustomButton';
+import {InputField} from 'components/@common/InputField/InputField';
+import {Typography} from 'components/@common/Typography/Typography';
+import {ScreenContainer} from 'components/ScreenContainer';
+import {CustomButton} from 'components/@common/CustomButton/CustomButton';
 import useDropdown from 'hooks/useDropdown';
 import CustomDropdown from 'components/@common/Dropdown/CustomDropdown';
-import { POST_WRITE_LIST } from 'constants/screens/MoimBoardStackScreens/PostList';
+import {POST_WRITE_LIST} from 'constants/screens/MoimBoardStackScreens/PostList';
 import ReadersBottomSheet from 'components/screens/MoimBoardStackScreens/ReadersBottomSheet';
 import usePost from 'hooks/queries/MoimBoard/usePost';
-import { MoimPostStackNavigationProp, MoimPostStackRouteProp } from 'navigators/types';
+import {
+  MoimPostStackNavigationProp,
+  MoimPostStackRouteProp,
+} from 'navigators/types';
+import {queryClient} from '../../containers/TanstackQueryContainer.tsx';
 
 const testImages = [
-  "https://images.unsplash.com/photo-1722648403966-922e95b2bd7e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1722265620783-12d44a721b5a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1722861315999-5de71ce7cdda?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5MXx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1722648403966-922e95b2bd7e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1722265620783-12d44a721b5a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1722861315999-5de71ce7cdda?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5MXx8fGVufDB8fHx8fA%3D%3D",
+  'https://images.unsplash.com/photo-1722648403966-922e95b2bd7e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1722265620783-12d44a721b5a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1722861315999-5de71ce7cdda?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5MXx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1722648403966-922e95b2bd7e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1722265620783-12d44a721b5a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1OHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1722861315999-5de71ce7cdda?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5MXx8fGVufDB8fHx8fA%3D%3D',
 ];
 
 interface MoimPostWriteScreenProps {
@@ -27,12 +38,10 @@ interface MoimPostWriteScreenProps {
   navigation: MoimPostStackNavigationProp;
 }
 
-const MoimPostWriteScreen = ({ 
-  route,
-  navigation
-}: MoimPostWriteScreenProps) => {
+const MoimPostWriteScreen = ({route, navigation}: MoimPostWriteScreenProps) => {
   const moimId = route?.params?.id;
-  const { isPressed, category, handleCategory, handleSelectedCategory } = useDropdown();
+  const {isPressed, category, handleCategory, handleSelectedCategory} =
+    useDropdown();
   // const [readers, setReaders] = useState('전체 대상');
   const [data, setData] = useState({
     title: '',
@@ -42,30 +51,43 @@ const MoimPostWriteScreen = ({
   const [isOpen, setIsOpen] = useState(false);
   const open = () => setIsOpen(true);
   const close = () => setIsOpen(false);
-  const { moimPostMutation } = usePost();
+  const {moimPostMutation} = usePost();
 
   const handleOnSubmit = () => {
     if (moimId && category && data.title) {
-      moimPostMutation.mutate({
-        moimId: moimId,
-        title: data.title,
-        content: data.content,
-        imageKeyNames: data.imageKeyNames,
-        postType: category?.key
-      }, {
-        onSuccess: (data) => {
-          navigation.navigate('MOIM_POST_DETAIL', { id: moimId, postId: data.moimPostId });
-          console.log('게시글 작성: ', data);
+      moimPostMutation.mutate(
+        {
+          moimId: moimId,
+          title: data.title,
+          content: data.content,
+          imageKeyNames: data.imageKeyNames,
+          postType: category?.key,
         },
-        onError: (error) => {
-          Toast.show({
-            type: 'error',
-            text1: error.response?.data.message,
-            visibilityTime: 2000,
-            position: 'bottom',
-          });
-        }
-      });
+        {
+          onSuccess: data => {
+            navigation.navigate('MOIM_POST_DETAIL', {
+              id: moimId,
+              postId: data.moimPostId,
+            });
+            queryClient.invalidateQueries({
+              queryKey: ['moim', 'post', category],
+            });
+            queryClient.invalidateQueries({
+              queryKey: ['moim', 'post', 'ALL'],
+            });
+
+            console.log('게시글 작성: ', data);
+          },
+          onError: error => {
+            Toast.show({
+              type: 'error',
+              text1: error.response?.data.message,
+              visibilityTime: 2000,
+              position: 'bottom',
+            });
+          },
+        },
+      );
     } else {
       Toast.show({
         type: 'error',
@@ -78,60 +100,99 @@ const MoimPostWriteScreen = ({
 
   return (
     <ScreenContainer>
-      <View className='mt-1' />
-        <CustomDropdown
-          isPressed={isPressed}
-          selectedMenu={category}
-          placeholder='카테고리 선택'
-          menuList={POST_WRITE_LIST.map(item => item.label)}
-          handleSelect={(label: any) => {
-            const selected = POST_WRITE_LIST.find(item => item.label === label);
-            console.log(selected);
-            handleSelectedCategory(selected);
-          }}
-          onPress={handleCategory}
-          height={160}
-        />
-        {category && category?.label === '공지사항' && <TouchableOpacity onPress={open} activeOpacity={0.8} className='flex flex-row border-0.5 border-gray-100 rounded-xl bg-gray-100 p-4'>
-          <Typography fontWeight='MEDIUM' className='text-sm text-gray-400'>전체 대상</Typography>
-          <Ionicons name='chevron-up-outline' color={'#535353'} size={15} style={{ marginLeft: 'auto' }} />
-        </TouchableOpacity>}
-        <InputField touched placeholder='제목을 입력해주세요.' value={data.title} onChangeText={text => setData(prev => ({ ...prev, title: text }))} />
-        <TextInput
-          placeholder='내용을 입력해주세요.' 
-          className='p-2 min-h-[300] max-h-[800] text-dark-800' 
-          multiline 
-          placeholderTextColor={'#72787F'}
-          textAlignVertical="top"
-          value={data.content}
-          onChangeText={text => setData(prev => ({ ...prev, content: text }))}
-        />
-        <View className='flex flex-col gap-y-3'>
-          <Typography fontWeight='LIGHT' className='text-gray-300 text-xs'>[ 모임 게시판 이용 안내 ]</Typography>
-          <Typography fontWeight='LIGHT' className='text-gray-300 text-xs'>게시판 주제에 맞지 않는 글, 커뮤니티 이용자에게 불쾌감을 줄 수 있는 내용, 차별 및 혐오표현, 유해 정보, 음란물, 불법 컨텐츠, 개인정보, 욕설, 명예쉐손 등이 포함된 게시글은 신고 대상입니다.</Typography>
-          <Typography fontWeight='LIGHT' className='text-gray-300 text-xs'>해당 게시글이 5번 이상 신고 되면 더 이상 게시판에 노출되지 않거나, 관리자 판단 하에 삭제 대상이 될 수 있습니다.</Typography>
-        </View>
-        {/* 이미지는 추후에 다시 연결 */}
-        <View className='flex flex-row items-center'>
-          <TouchableOpacity activeOpacity={0.8}>
-            <Ionicons name='camera' size={30} color={'#9EA4AA'} style={{padding: 10}}/>
-          </TouchableOpacity>
-          <FlatList
-            horizontal
-            data={testImages}
-            contentContainerStyle={{ marginLeft: 20 }}
-            renderItem={({item}) => (
-                <View className='w-[80] h-[100]'>
-                  <Image source={{uri: item}} className='w-full h-full rounded-2xl' />
-                  <Pressable className='flex flex-col items-center justify-center absolute bottom-0 w-[80] bg-white h-2/5 rounded-b-2xl border-[1px] border-gray-200'>
-                    <Ionicons name="trash" size={15} color={'#9EA4AA'} />
-                  </Pressable>
-                </View>
-            )}
-            ItemSeparatorComponent={() => <View className='w-2' />}
+      <View className="mt-1" />
+      <CustomDropdown
+        isPressed={isPressed}
+        selectedMenu={category}
+        placeholder="카테고리 선택"
+        menuList={POST_WRITE_LIST.map(item => item.label)}
+        handleSelect={(label: any) => {
+          const selected = POST_WRITE_LIST.find(item => item.label === label);
+          console.log(selected);
+          handleSelectedCategory(selected);
+        }}
+        onPress={handleCategory}
+        height={160}
+      />
+      {category && category?.label === '공지사항' && (
+        <TouchableOpacity
+          onPress={open}
+          activeOpacity={0.8}
+          className="flex flex-row border-0.5 border-gray-100 rounded-xl bg-gray-100 p-4">
+          <Typography fontWeight="MEDIUM" className="text-sm text-gray-400">
+            전체 대상
+          </Typography>
+          <Ionicons
+            name="chevron-up-outline"
+            color={'#535353'}
+            size={15}
+            style={{marginLeft: 'auto'}}
           />
+        </TouchableOpacity>
+      )}
+      <InputField
+        touched
+        placeholder="제목을 입력해주세요."
+        value={data.title}
+        onChangeText={text => setData(prev => ({...prev, title: text}))}
+      />
+      <TextInput
+        placeholder="내용을 입력해주세요."
+        className="p-2 min-h-[300] max-h-[800] text-dark-800"
+        multiline
+        placeholderTextColor={'#72787F'}
+        textAlignVertical="top"
+        value={data.content}
+        onChangeText={text => setData(prev => ({...prev, content: text}))}
+      />
+      <View className="flex flex-col gap-y-3">
+        <Typography fontWeight="LIGHT" className="text-gray-300 text-xs">
+          [ 모임 게시판 이용 안내 ]
+        </Typography>
+        <Typography fontWeight="LIGHT" className="text-gray-300 text-xs">
+          게시판 주제에 맞지 않는 글, 커뮤니티 이용자에게 불쾌감을 줄 수 있는
+          내용, 차별 및 혐오표현, 유해 정보, 음란물, 불법 컨텐츠, 개인정보,
+          욕설, 명예쉐손 등이 포함된 게시글은 신고 대상입니다.
+        </Typography>
+        <Typography fontWeight="LIGHT" className="text-gray-300 text-xs">
+          해당 게시글이 5번 이상 신고 되면 더 이상 게시판에 노출되지 않거나,
+          관리자 판단 하에 삭제 대상이 될 수 있습니다.
+        </Typography>
       </View>
-      <CustomButton onPress={handleOnSubmit} label='게시하기' textStyle='text-white text-base font-bold' className='mt-3' />
+      {/* 이미지는 추후에 다시 연결 */}
+      <View className="flex flex-row items-center">
+        <TouchableOpacity activeOpacity={0.8}>
+          <Ionicons
+            name="camera"
+            size={30}
+            color={'#9EA4AA'}
+            style={{padding: 10}}
+          />
+        </TouchableOpacity>
+        <FlatList
+          horizontal
+          data={testImages}
+          contentContainerStyle={{marginLeft: 20}}
+          renderItem={({item}) => (
+            <View className="w-[80] h-[100]">
+              <Image
+                source={{uri: item}}
+                className="w-full h-full rounded-2xl"
+              />
+              <Pressable className="flex flex-col items-center justify-center absolute bottom-0 w-[80] bg-white h-2/5 rounded-b-2xl border-[1px] border-gray-200">
+                <Ionicons name="trash" size={15} color={'#9EA4AA'} />
+              </Pressable>
+            </View>
+          )}
+          ItemSeparatorComponent={() => <View className="w-2" />}
+        />
+      </View>
+      <CustomButton
+        onPress={handleOnSubmit}
+        label="게시하기"
+        textStyle="text-white text-base font-bold"
+        className="mt-3"
+      />
       <ReadersBottomSheet isOpen={isOpen} onOpen={open} onClose={close} />
     </ScreenContainer>
   );

2995e31599363abd55d55e4572598bf62744d979
daindaind <cdin23@naver.com>
2024-08-21 23:28:23 +0900

    Feat: 모임 가입 관리 API 연결 및 수정

--- src/hooks/queries/MoimManagement/useMoimManagement.ts
+++ src/hooks/queries/MoimManagement/useMoimManagement.ts
@@ -1,111 +1,141 @@
-import { InfiniteData, QueryKey, useInfiniteQuery, UseInfiniteQueryOptions, useMutation } from "@tanstack/react-query";
-import { acceptMoimJoinRequest, getMoimMembers, getMoimRequestUsers, updateMoimAuthorities, updateMoimInfo } from "apis/moimManage";
-import { TGetMoimMembers, TMoimRequestUsers } from "types/dtos/moimManage";
-import { ResponseError, UseMutationCustomOptions } from "types/mutations/common";
+import {
+  InfiniteData,
+  QueryKey,
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  useMutation,
+} from '@tanstack/react-query';
+import {
+  acceptMoimJoinRequest,
+  getMoimMembers,
+  getMoimRequestUsers,
+  rejectMoimJoinRequest,
+  updateMoimAuthorities,
+  updateMoimInfo,
+} from 'apis/moimManage';
+import {TGetMoimMembers, TMoimRequestUsers} from 'types/dtos/moimManage';
+import {ResponseError, UseMutationCustomOptions} from 'types/mutations/common';
 
-function useGetInfinityMoimRequest (
-    moimId: number,
-    queryOptions?: UseInfiniteQueryOptions<
-        TMoimRequestUsers,
-        ResponseError,
-        InfiniteData<TMoimRequestUsers, number>,
-        TMoimRequestUsers,
-        QueryKey,
-        number
-    >
+function useGetInfinityMoimRequest(
+  moimId: number,
+  queryOptions?: UseInfiniteQueryOptions<
+    TMoimRequestUsers,
+    ResponseError,
+    InfiniteData<TMoimRequestUsers, number>,
+    TMoimRequestUsers,
+    QueryKey,
+    number
+  >,
 ) {
-    return useInfiniteQuery({
-        queryFn: ({pageParam}) => getMoimRequestUsers({
-            moimId,
-            cursor: pageParam,
-            take: 10
-        }),
-        queryKey: ['moimRequests'],
-        initialPageParam: 1,
-        getNextPageParam: (lastPage) => {
-            return lastPage.hasNext ? lastPage.nextCursor : undefined;
-        },
-        ...queryOptions
-    });
+  return useInfiniteQuery({
+    queryFn: ({pageParam}) =>
+      getMoimRequestUsers({
+        moimId,
+        cursor: pageParam,
+        take: 10,
+      }),
+    queryKey: ['moimRequests', moimId],
+    initialPageParam: 1,
+    getNextPageParam: lastPage => {
+      return lastPage.hasNext ? lastPage.nextCursor : undefined;
+    },
+    ...queryOptions,
+  });
 }
 
 function useGetInfinityMoimMembers(
-    moimId: number,
-    queryOptions?: UseInfiniteQueryOptions<
-        TGetMoimMembers,
-        ResponseError,
-        InfiniteData<TGetMoimMembers, number>,
-        TGetMoimMembers,
-        QueryKey,
-        number
-    >
+  moimId: number,
+  queryOptions?: UseInfiniteQueryOptions<
+    TGetMoimMembers,
+    ResponseError,
+    InfiniteData<TGetMoimMembers, number>,
+    TGetMoimMembers,
+    QueryKey,
+    number
+  >,
 ) {
-    return useInfiniteQuery({
-        queryFn: ({pageParam}) => getMoimMembers({
-            moimId,
-            cursor: pageParam,
-            take: 30
-        }),
-        queryKey: ['moimMembers'],
-        initialPageParam: 1,
-        getNextPageParam: (lastPage) => {
-            return lastPage.hasNext ? lastPage.nextCursor : undefined;
-        },
-        ...queryOptions
-    });
+  return useInfiniteQuery({
+    queryFn: ({pageParam}) =>
+      getMoimMembers({
+        moimId,
+        cursor: pageParam,
+        take: 30,
+      }),
+    queryKey: ['moimMembers'],
+    initialPageParam: 1,
+    getNextPageParam: lastPage => {
+      return lastPage.hasNext ? lastPage.nextCursor : undefined;
+    },
+    ...queryOptions,
+  });
 }
 
 function useUpdateMoimAuthorities(mutationOptions?: UseMutationCustomOptions) {
-    return useMutation({
-        mutationFn: updateMoimAuthorities,
-        onSuccess: data => {
-            console.log(data);
-        },
-        onError: error => {
-            console.error(error);
-        },
-        ...mutationOptions
-    });
+  return useMutation({
+    mutationFn: updateMoimAuthorities,
+    onSuccess: data => {
+      console.log(data);
+    },
+    onError: error => {
+      console.error(error);
+    },
+    ...mutationOptions,
+  });
 }
 
 function useAcceptMoimJoinRequest(mutationOptions?: UseMutationCustomOptions) {
-    return useMutation({
-        mutationFn: acceptMoimJoinRequest,
-        onSuccess: data => {
-            console.log(data);
-        },
-        onError: error => {
-            console.error(error);
-        },
-        ...mutationOptions
-    });
+  return useMutation({
+    mutationFn: acceptMoimJoinRequest,
+    onSuccess: data => {
+      console.log(data);
+    },
+    onError: error => {
+      console.error(error);
+    },
+    ...mutationOptions,
+  });
 }
 
 function useUpdateMoimInfo(mutationOptions?: UseMutationCustomOptions) {
-    return useMutation({
-        mutationFn: updateMoimInfo,
-        onSuccess: data => {
-            console.log(data);
-        },
-        onError: error => {
-            console.error(error);
-        },
-        ...mutationOptions
-    });
+  return useMutation({
+    mutationFn: updateMoimInfo,
+    onSuccess: data => {
+      console.log(data);
+    },
+    onError: error => {
+      console.error(error);
+    },
+    ...mutationOptions,
+  });
+}
+
+function useRejectMoimRequest(mutationOptions?: UseMutationCustomOptions) {
+  return useMutation({
+    mutationFn: rejectMoimJoinRequest,
+    onSuccess: data => {
+      console.log(data);
+    },
+    onError: error => {
+      console.error(error);
+    },
+    ...mutationOptions,
+  });
 }
 
 function useMoimManagment() {
-    const updateMoimAuthoritiesMutation = useUpdateMoimAuthorities();
-    const acceptMoimJoinRequestMutation = useAcceptMoimJoinRequest();
-    const updateMoimInfoMutation = useUpdateMoimInfo();
+  const updateMoimAuthoritiesMutation = useUpdateMoimAuthorities();
+  const acceptMoimJoinRequestMutation = useAcceptMoimJoinRequest();
+  const updateMoimInfoMutation = useUpdateMoimInfo();
+  const rejectMoimJoinRequestMutation = useRejectMoimRequest();
 
-    return {
-        useGetInfinityMoimRequest,
-        useGetInfinityMoimMembers,
-        updateMoimAuthoritiesMutation,
-        acceptMoimJoinRequestMutation,
-        updateMoimInfoMutation
-    };
+  return {
+    useGetInfinityMoimRequest,
+    useGetInfinityMoimMembers,
+    updateMoimAuthoritiesMutation,
+    acceptMoimJoinRequestMutation,
+    updateMoimInfoMutation,
+    rejectMoimJoinRequestMutation,
+  };
 }
 
 export default useMoimManagment;
--- src/screens/MoimManagementScreens/JoinManageScreen.tsx
+++ src/screens/MoimManagementScreens/JoinManageScreen.tsx
@@ -1,11 +1,15 @@
-import { FlatList, TouchableOpacity, View } from 'react-native';
+import {useState} from 'react';
+import {FlatList, TouchableOpacity, View} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { useState } from 'react';
+import Toast from 'react-native-toast-message';
+
 import Avatar from 'components/@common/Avatar/Avatar';
-import { InputField } from 'components/@common/InputField/InputField';
-import { Typography } from 'components/@common/Typography/Typography';
-import { MoimManagementRouteProp } from 'navigators/types';
+import {InputField} from 'components/@common/InputField/InputField';
+import {Typography} from 'components/@common/Typography/Typography';
+
+import {MoimManagementRouteProp} from 'navigators/types';
 import useMoimManagment from 'hooks/queries/MoimManagement/useMoimManagement';
+import {queryClient} from 'containers/TanstackQueryContainer';
 
 interface JoinManageScreenProps {
   route: MoimManagementRouteProp;
@@ -13,8 +17,12 @@ interface JoinManageScreenProps {
 
 const JoinManageScreen = ({route}: JoinManageScreenProps) => {
   const moimId = route?.params?.id;
-  const { useGetInfinityMoimRequest } = useMoimManagment();
-  const { 
+  const {
+    useGetInfinityMoimRequest,
+    acceptMoimJoinRequestMutation,
+    rejectMoimJoinRequestMutation,
+  } = useMoimManagment();
+  const {
     data: moimRequests,
     fetchNextPage,
     hasNextPage,
@@ -37,12 +45,86 @@ const JoinManageScreen = ({route}: JoinManageScreenProps) => {
     setIsRefreshing(false);
   };
 
+  const handleAcceptRequest = (userId: number) => {
+    if (moimId && userId) {
+      acceptMoimJoinRequestMutation.mutate(
+        {
+          moimId,
+          userId,
+        },
+        {
+          onSuccess: () => {
+            Toast.show({
+              type: 'success',
+              text1: '신청이 승인되었습니다.',
+              visibilityTime: 2000,
+              position: 'bottom',
+            });
+          },
+          onError: error => {
+            console.error(error.response);
+            Toast.show({
+              type: 'error',
+              text1:
+                error.response?.data.message ||
+                '신청 승인 중 에러가 발생했습니다.',
+              visibilityTime: 2000,
+              position: 'bottom',
+            });
+          },
+          onSettled: () => {
+            queryClient.invalidateQueries({
+              queryKey: ['moimRequests', moimId],
+            });
+          },
+        },
+      );
+    }
+  };
+
+  const handleRejectRequest = (userId: number) => {
+    if (moimId && userId) {
+      rejectMoimJoinRequestMutation.mutate(
+        {
+          moimId,
+          userId,
+        },
+        {
+          onSuccess: () => {
+            Toast.show({
+              type: 'success',
+              text1: '신청이 반려되었습니다.',
+              visibilityTime: 2000,
+              position: 'bottom',
+            });
+          },
+          onError: error => {
+            console.error(error.response);
+            Toast.show({
+              type: 'error',
+              text1:
+                error.response?.data.message ||
+                '신청 반려 중 에러가 발생했습니다.',
+              visibilityTime: 2000,
+              position: 'bottom',
+            });
+          },
+          onSettled: () => {
+            queryClient.invalidateQueries({
+              queryKey: ['moimRequests', moimId],
+            });
+          },
+        },
+      );
+    }
+  };
+
   if (isPending) {
-    return <Typography fontWeight='MEDIUM'>로딩 중</Typography>;
+    return <Typography fontWeight="MEDIUM">로딩 중</Typography>;
   }
 
   if (isError) {
-    return <Typography fontWeight='MEDIUM'>에러</Typography>;
+    return <Typography fontWeight="MEDIUM">에러</Typography>;
   }
 
   const renderHeader = () => (
@@ -51,39 +133,40 @@ const JoinManageScreen = ({route}: JoinManageScreenProps) => {
         <InputField touched placeholder="멤버 검색" />
       </View>
       <TouchableOpacity>
-        <Ionicons name="search" size={27} color={"#1D2002"} />
+        <Ionicons name="search" size={27} color={'#1D2002'} />
       </TouchableOpacity>
     </View>
   );
 
   return (
-    <View className='flex-1 bg-white p-4 pt-0'>
+    <View className="flex-1 bg-white p-4 pt-0">
       <FlatList
         ListHeaderComponent={renderHeader}
-        data={moimRequests.pages.flatMap((page) => page.userPreviewDTOList)}
-        renderItem={({ item }) => (
-          <View key={item.userId} className="flex flex-row items-center">
+        data={moimRequests.pages.flatMap(page => page.userPreviewDTOList)}
+        renderItem={({item}) => (
+          <View key={item.userId} className="flex flex-row items-center py-4">
             <Avatar uri={item.imageKeyName} />
             <Typography
               fontWeight="MEDIUM"
-              className="text-dark-800 text-sm ml-3"
-            >
+              className="text-dark-800 text-sm ml-3">
               {item.nickname}
             </Typography>
             <View className="flex flex-row ml-auto">
-              <TouchableOpacity className="p-2 px-4 rounded-xl bg-gray-200 mr-2">
+              <TouchableOpacity
+                className="p-2 px-4 rounded-xl bg-gray-200 mr-2"
+                onPress={() => handleRejectRequest(item.userId)}>
                 <Typography
                   fontWeight="MEDIUM"
-                  className="text-gray-600 text-xs"
-                >
+                  className="text-gray-600 text-xs">
                   반려
                 </Typography>
               </TouchableOpacity>
-              <TouchableOpacity className="p-2 px-4 rounded-xl bg-gray-200">
+              <TouchableOpacity
+                className="p-2 px-4 rounded-xl bg-gray-200"
+                onPress={() => handleAcceptRequest(item.userId)}>
                 <Typography
                   fontWeight="MEDIUM"
-                  className="text-gray-600 text-xs"
-                >
+                  className="text-gray-600 text-xs">
                   승인
                 </Typography>
               </TouchableOpacity>

395ccbc037f0b8e639906ef3a56899ea4ad5506e
daindaind <cdin23@naver.com>
2024-08-22 07:04:42 +0900

    Bug: 드롭다운 컴포넌트 선택하면 닫히도록 로직 수정

--- src/components/@common/Dropdown/CustomDropdown.tsx
+++ src/components/@common/Dropdown/CustomDropdown.tsx
@@ -1,72 +1,93 @@
-import { useEffect, useRef } from 'react';
-import { Animated, Easing, ScrollView, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import {useEffect, useRef} from 'react';
+import {
+  Animated,
+  Easing,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Typography } from '../Typography/Typography';
+import {Typography} from '../Typography/Typography';
 
-interface CustomDropdownProps extends TouchableOpacityProps {
-    isPressed: boolean;
-    selectedMenu: any;
-    placeholder?: string;
-    menuList: any[];
-    handleSelect: (item: any) => void;
-    height?: number;
+interface CustomDropdownProps {
+  isPressed: boolean;
+  selectedMenu: any;
+  placeholder?: string;
+  menuList: any[];
+  handleSelect: (item: any) => void;
+  height?: number;
+  onPress: () => void;
 }
 
 const CustomDropdown = ({
-    isPressed,
-    selectedMenu,
-    placeholder,
-    menuList,
-    handleSelect,
-    height = 300,
-    ...props
+  isPressed,
+  selectedMenu,
+  placeholder,
+  menuList,
+  handleSelect,
+  height = 300,
+  onPress,
 }: CustomDropdownProps) => {
-    const dropdownHeight = useRef(new Animated.Value(0)).current;
+  const dropdownHeight = useRef(new Animated.Value(0)).current;
 
-    useEffect(() => {
-        Animated.timing(dropdownHeight, {
-            toValue: isPressed ? height : 0,
-            duration: 300,
-            easing: Easing.ease,
-            useNativeDriver: false,
-        }).start();
-    }, [isPressed]);
+  useEffect(() => {
+    Animated.timing(dropdownHeight, {
+      toValue: isPressed ? height : 0,
+      duration: 300,
+      easing: Easing.ease,
+      useNativeDriver: false,
+    }).start();
+  }, [isPressed]);
 
-    const padding = dropdownHeight.interpolate({
-        inputRange: [0, 100],
-        outputRange: [0, 12],
-    });
-    
-    return (
-        <View className='flex flex-col'>
+  const padding = dropdownHeight.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 12],
+  });
+
+  return (
+    <View className="flex flex-col">
+      <TouchableOpacity
+        onPress={onPress}
+        activeOpacity={0.8}
+        className="flex flex-row border-0.5 border-gray-200 rounded-xl bg-gray-100 p-4">
+        <Typography
+          fontWeight="MEDIUM"
+          className={selectedMenu ? 'text-dark-800' : 'text-gray-500'}>
+          {selectedMenu ? selectedMenu.label : placeholder}
+        </Typography>
+        <Ionicons
+          name="chevron-expand"
+          style={{marginLeft: 'auto'}}
+          size={15}
+          color={'#535353'}
+        />
+      </TouchableOpacity>
+
+      <Animated.View
+        style={{
+          height: dropdownHeight,
+          paddingHorizontal: 12,
+          paddingVertical: padding,
+        }}
+        className="flex flex-col bg-gray-100 rounded-xl pt-0 gap-y-2 mt-2">
+        <ScrollView nestedScrollEnabled>
+          {menuList.map((item, index) => (
             <TouchableOpacity
-                {...props}
-                activeOpacity={0.8} 
-                className='flex flex-row border-0.5 border-gray-200 rounded-xl bg-gray-100 p-4'
-            >
-                <Typography 
-                    fontWeight='MEDIUM' 
-                    className={selectedMenu ? 'text-dark-800' : 'text-gray-500'}
-                >
-                    {selectedMenu ? selectedMenu.label : placeholder }
-                </Typography>
-                <Ionicons name="chevron-expand" style={{marginLeft: 'auto'}} size={15} color={'#535353'} />
+              key={index}
+              onPress={() => {
+                handleSelect(item);
+                onPress();
+              }}
+              className="p-2">
+              <Typography fontWeight="MEDIUM" className="text-dark-800 text-sm">
+                {item}
+              </Typography>
             </TouchableOpacity>
-
-            <Animated.View 
-                style={{ height: dropdownHeight, paddingHorizontal: 12, paddingVertical: padding }}
-                className='flex flex-col bg-gray-100 rounded-xl pt-0 gap-y-2 mt-2'
-            >
-                <ScrollView nestedScrollEnabled>
-                    {menuList.map((item, index) => (
-                        <TouchableOpacity key={index} onPress={() => handleSelect(item)} className='p-2'>
-                            <Typography fontWeight='MEDIUM' className='text-dark-800 text-sm'>{item}</Typography>
-                        </TouchableOpacity>
-                    ))}
-                </ScrollView>
-            </Animated.View>
-        </View>
-    );
+          ))}
+        </ScrollView>
+      </Animated.View>
+    </View>
+  );
 };
 
 export default CustomDropdown;
